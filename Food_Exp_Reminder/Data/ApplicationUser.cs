6337bcffeb1aecff6d6a044b352f627d846b0d9b
Nafim3 <148070846+Nafim3@users.noreply.github.com>
2025-03-16 23:52:55 +0000

    Database Issue, added login, signup pages

--- Food_Exp_Reminder/Data/ApplicationUser.cs
+++ Food_Exp_Reminder/Data/ApplicationUser.cs
@@ -1,10 +1,15 @@
 using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Food_Exp_Reminder.Data
 {
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        [NotMapped]
+        public string? Password { get; set; }
     }
 
 }
